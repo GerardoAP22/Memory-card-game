cd1a7b956e688229c1e56d66edaed2793a04292e
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-05-08 16:57:02 -0400

    game Layout set

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+
+
+
+/*---variables-----*/
+
 // Get all memory cards
 const memoryCards = document.querySelectorAll('.memory-card');
+//select the reset button 
+const resetButton = document.getElementById('reset-button')
 
+
+
+/*-----Functions------*/
 // Add click event listener to each memory card
 memoryCards.forEach(card => {
   card.addEventListener('click', () => {
     card.classList.toggle('flipped');
   });
 });
+//now once the cards are clicked switches to the other card
+
+resetButton.addEventListener('click', function(){
+    resetGame();
+});
+
+// funtion resetGame(){
+
+// }
+function shuffle(array) {
+
+}
+
+
+
