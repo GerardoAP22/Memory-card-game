afde28b09bd29f2293c68da917392d19392498be
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-05-09 15:14:21 -0400

    added a reset button & addes the shuffle of the cards

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-
-
-
 /*---variables-----*/
 
-// Get all memory cards
+const currentScore = document.querySelector('.score');
 const memoryCards = document.querySelectorAll('.memory-card');
-//select the reset button 
-const resetButton = document.getElementById('reset-button')
+const cardNames = ['#goku','#gon', '#kilua', '#luffy','#vegeta','#zoro'];
+let randomPosition = [];
 
-
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let flippedCards = 0;
+const order = 'order';
 
 /*-----Functions------*/
-// Add click event listener to each memory card
-memoryCards.forEach(card => {
+// startGame(){
+//   this.cardToCheck =null;
+//   this.matchedCards = [];
+//   this.busy =true;
+// }
+
+// Fisher-Yates Shuffle Function
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  console.log(currentIndex);
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+for(let i = 0; i < cardNames.length; i++){
+  randomPosition.push(cardNames[i]);
+}
+
+randomPosition = shuffle(randomPosition.concat(randomPosition)); // Shuffle the cards and duplicate them
+
+memoryCards.forEach((card, index) => {
+  card.dataset.name = randomPosition[index].substring(1); // Assign shuffled card names
+  card.children[1].src = `images/${randomPosition[index].substring(1)}.jpg`; // Assign shuffled card images
   card.addEventListener('click', () => {
     card.classList.toggle('flipped');
   });
 });
-//now once the cards are clicked switches to the other card
 
-resetButton.addEventListener('click', function(){
-    resetGame();
+// resets the game!!
+document.getElementById('reset-game').addEventListener('click',function(){
+    window.location.reload();
+    return  false;
 });
-
-// funtion resetGame(){
-
-// }
-function shuffle(array) {
-
-}
-
-
-
