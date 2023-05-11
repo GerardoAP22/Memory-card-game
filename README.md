471fad520b3d2ca0c8751da38147fb81a7605420
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-05-11 16:32:31 -0400

    mvps met added sounds to first and second click with a victory sound

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Memory-Card-Game
+
+
+## description
+
+- The game starts with all the cards face down and player has to turn over two cards. If the two cards have the same picture, then they keep the cards, otherwise they turn the cards face down again. Win the game when all the card are matched.
+
+## initial image
+
+![](images/memor-game-cards.png)
+
+
+ ## Technologies Used:
+
+ HTML, JavaScript, CSS.
+
+
+ ## Getting Started: 
+  - game deployed!
+
+
+ https://eclectic-shortbread-27bbb8.netlify.app
+
+ ## frequent commits
+Commits on May 11, 2023
+ mvp is in
+ 
+Commits on May 10, 2023
+lots of modification
+
+Commits on May 9, 2023
+added a reset button & addes the shuffle of the cards
+
+Commits on May 8, 2023
+game Layout set
+
+Commits on May 7, 2023
+proposal final
+ 
+Commits on May 6, 2023
+personal test
+ 
+Commits on May 5, 2023
+first commit
+
--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 const cards = document.querySelectorAll('.memory-card');
+//cards hold all the values of the cards under the class called memory-card
+
 const resetButton = document.getElementById('reset-button');
+// id under the name reset-button gets stored in the variable called resetButton
+const zoroSoundPath = "audio-sounds/zoro.mp3";
+const zoroAudio  = new Audio(zoroSoundPath)
+zoroAudio.volume = 0.1;
+// These lines create an Audio object for the sound file at the path audio-sounds/zoro.mp3, sets its volume to 0.1, and stores it in the zoroAudio variable
+//adjust the volume of every sound by adding volume^^^
+const luffySoundPath = "audio-sounds/luffy.mp3";
+const luffyAudio = new Audio(luffySoundPath)
+//  creats a new path for the audio luffy
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
-//we lock the bord after 2 cards are being check for matche so we cant turn more cards while they are being checkMatch
+//we lock the board after 2 cards are being check for matche so we cant turn more cards while they are being checkMatch
 
+
+
+  // cards.addEventListener('click', function(){
+  //   audio.play();
+  //   console.log("Just clicked the button!");
+  // });
+
+//flip card function that checks which card is being fliped first or secodn, and plays the assigned aout depending on whic card is being clicked
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
 
   this.classList.add('flip')
+  
 
   if (!hasFlippedCard) {
     //first Click
     hasFlippedCard = true;
     firstCard = this;
+    zoroAudio.play();
     return;
   } 
     //second click
     hasFlippedCard = false;
     secondCard = this;
-
+  luffyAudio.play();
     checkForMatch();
   }
 
@@ -30,6 +51,7 @@ function checkForMatch() {
 //ternary operator //lets you write the if statment in less lines
 //isMatch is condition ? true : false
   isMatch ? disableCards() : unflipCards();
+
 }
 
 function disableCards() {
@@ -38,7 +60,17 @@ function disableCards() {
   secondCard.removeEventListener('click', flipCard);
   //if its a match it stops the card to be clicked again
   resetBoard();
-}
+    // Check if all cards are matched
+    const matchedCards = document.querySelectorAll('.flip');
+    if (matchedCards.length === cards.length) {
+      setTimeout(() => {//given a set timer to way a 1.5 seconds before it plays
+        // All cards are matched, play the audio
+        const allMatchedAudio = new Audio('audio-sounds/victory.mp3');
+        allMatchedAudio.play();
+      }, 1500);
+    }
+  }
+
 //gives time to check if the cards match
 function unflipCards() {
   lockBoard = true;
@@ -49,12 +81,12 @@ function unflipCards() {
   }, 1500);
   //if cards dont match it will unflip them after 1.5s
 
-}
+}//the state of the board eveytime there is a click
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 } 
-
+//randomizing the position of each card for every new game
 (function shuffle(){
   cards.forEach(card => {
     let randomPos = Math.floor (Math.random() * 12)
@@ -65,6 +97,7 @@ function resetBoard() {
 document.getElementById('reset-game').addEventListener('click',function(){
   window.location.reload();
   return  false;
-});
+});//button that restarts the game from the begining
+
 //calls the function that flips name
 cards.forEach(card => card.addEventListener('click', flipCard))
